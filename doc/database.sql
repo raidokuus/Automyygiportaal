ce3b300f9b2b9b8b3611bfde0f3694ec4497a775
Kaspar Naaber <kaspar@fraktal.ee>
2014-02-07 12:49:35 +0200

    Update SQL Dump
    Change database name and add car table

--- doc/database.sql
+++ doc/database.sql
@@ -1,24 +1,38 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.2.2
+-- version 4.0.6
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 02, 2013 at 10:20 AM
--- Server version: 5.5.27
--- PHP Version: 5.4.7
+-- Generation Time: Feb 07, 2014 at 11:46 AM
+-- Server version: 5.5.33
+-- PHP Version: 5.5.3
 
 SET FOREIGN_KEY_CHECKS=0;
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+--
+-- Database: `autoportaal`
+--
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+-- --------------------------------------------------------
 
 --
--- Database: `halo`
+-- Table structure for table `car`
 --
+
+DROP TABLE IF EXISTS `car`;
+CREATE TABLE `car` (
+  `car_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `car_title` varchar(255) COLLATE utf8mb4_estonian_ci NOT NULL,
+  `car_info` text COLLATE utf8mb4_estonian_ci NOT NULL,
+  `car_vin` int(11) NOT NULL,
+  `car_image` longblob NOT NULL,
+  `car_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`car_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_estonian_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -27,7 +41,7 @@ SET time_zone = "+00:00";
 --
 
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
+CREATE TABLE `user` (
   `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(25) NOT NULL,
   `password` varchar(255) NOT NULL,
@@ -42,7 +56,3 @@ CREATE TABLE IF NOT EXISTS `user` (
 INSERT INTO `user` (`user_id`, `username`, `password`, `deleted`) VALUES
 (1, 'demo', 'demo', 0);
 SET FOREIGN_KEY_CHECKS=1;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
