4242583314b1f44c2865dcd502c941d1877d504c
Kaspar Naaber <kaspar@fraktal.ee>
2014-02-07 13:35:24 +0200

    List cars on welcome page

--- views/welcome/welcome_index.php
+++ views/welcome/welcome_index.php
@@ -1,23 +1,18 @@
-<div class="container">
+<div class="container container-welcome">
   <div class="row">
-    <h2 class="text-center">Automüügiportaal </h2>
-        <hr/>
+    <h2 class="text-center">Automüügiportaal</h2>
+    <hr/>
   </div>
-    <div class="row">
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-                <h4>
-                    Post Thumbnail List
-                    <span class="label label-info info">
-                        <span data-toggle="tooltip" title="viewed">257 <b class="glyphicon glyphicon-eye-open"></b></span>
-                        <span data-toggle="tooltip" title="viewed">3 <b class="glyphicon glyphicon-star"></b></span>
-                        <span data-toggle="tooltip" title="Bootstrap version">3.0.3</span>
-                    </span>
-                </h4>
-                <img src="http://www.remabledesigns.com/bootsnipp/img/201402031158.png" alt="...">
-                <a href="http://bootsnipp.com/snippets/featured/post-thumbnail-list" class="btn btn-primary col-xs-12" role="button">View Snippet</a>
-                <div class="clearfix"></div>
-            </div>
+  <div class="row">
+    <?foreach( $cars as $car ):?>
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+          <h4><?=$car['car_title']?></h4>
+          <img src="<?=$car['car_img']?>" alt="<?=$car['car_title']?>">
+          <a href="http://bootsnipp.com/snippets/featured/post-thumbnail-list" class="btn btn-primary col-xs-12" role="button">View car</a>
+          <div class="clearfix"></div>
         </div>
-    </div>
+      </div>
+    <?endforeach?>
+  </div>
 </div>
