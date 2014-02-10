ee603747175d44f61b7349c7c65e288fd0ed4d7b
Lennu <lenard.latte@khk.ee>
2014-02-10 10:31:50 +0200

    Update database dump

--- doc/database.sql
+++ doc/database.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.6
+-- version 4.0.9
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 07, 2014 at 12:45 PM
--- Server version: 5.5.33
--- PHP Version: 5.5.3
+-- Generation Time: Feb 10, 2014 at 09:31 AM
+-- Server version: 5.5.34
+-- PHP Version: 5.4.22
 
 SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -22,7 +22,7 @@ SET time_zone = "+00:00";
 --
 
 DROP TABLE IF EXISTS `car`;
-CREATE TABLE `car` (
+CREATE TABLE IF NOT EXISTS `car` (
   `car_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `car_title` varchar(255) COLLATE utf8mb4_estonian_ci NOT NULL,
   `car_info` text COLLATE utf8mb4_estonian_ci NOT NULL,
@@ -50,18 +50,21 @@ INSERT INTO `car` (`car_id`, `car_title`, `car_info`, `car_vin`, `car_img`, `car
 --
 
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
+CREATE TABLE IF NOT EXISTS `user` (
   `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(25) NOT NULL,
-  `password` varchar(255) NOT NULL,
+  `password` varchar(32) NOT NULL,
   `deleted` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `first_name` varchar(32) NOT NULL,
+  `last_name` varchar(32) NOT NULL,
+  `email` varchar(1024) NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`user_id`, `username`, `password`, `deleted`) VALUES
-(1, 'demo', 'demo', 0);
+INSERT INTO `user` (`user_id`, `username`, `password`, `deleted`, `first_name`, `last_name`, `email`) VALUES
+(1, 'demo', 'demo', 0, '', '', '');
 SET FOREIGN_KEY_CHECKS=1;
