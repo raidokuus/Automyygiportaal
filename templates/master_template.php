c0574453ae1895880b3ff2783f183333cf9eff19
Lennu <lenard.latte@khk.ee>
2014-02-18 22:43:13 +0200

    Add search (Work In Progress)

--- templates/master_template.php
+++ templates/master_template.php
@@ -45,7 +45,6 @@
         <div class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
                 <li <?= $controller == 'cars' ? 'class="active"' : '' ?>><a href="<?=BASE_URL?>cars">Autod</a></li>
-                <li><a href="<?=BASE_URL?>search">Otsing</a></li>
                 <!-- 				<li><a href="#contact">Contact</a></li>
                                 <li class="dropdown">
                                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
