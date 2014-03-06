e1a294428ef487aa586b913f70337ee09efc9988
tanel.kolga <tanel.kolga@khk.ee>
2014-03-06 11:48:40 +0200

    Henno help

--- views/welcome/welcome_index.php
+++ views/welcome/welcome_index.php
@@ -22,9 +22,9 @@
     </div>
     <div id="rightside">
         <div class="row">
-            <form method="get">
+            <form action="search" method="post">
                 <label class="control-label">Keretüüp:</label>
-                    <select>
+                    <select name = "body_type_id">
                     <option selected="selected" disabled="disabled">-kõik-</option>
                     <? foreach ($body_type_index as $body_type): ?>
                         <option value="<?= $body_type['body_type_id'] ?>"><?= $body_type['body_type_name'] ?></option>
@@ -34,7 +34,7 @@
                     </br>
 
                     <label class="control-label">Mark:</label>
-                    <select onchange="get_models()" id="make">
+                    <select name = "make_id" onchange="get_models()" id="make">
                         <option selected="selected" disabled="disabled">-kõik-</option>
                         <? foreach ($make_index as $make): ?>
                             <option value="<?= $make['make_id'] ?>"><?= $make['make_name'] ?></option>
@@ -43,7 +43,7 @@
                     </br>
 
                     <label class="control-label">Mudel:</label>
-                    <select id="model">
+                    <select name = "make_id" id="model">
                         <option selected="selected" disabled="disabled">-kõik-</option>
                         <? foreach ($make_s as $make): ?>
                             <option value="<?= $make['make_id'] ?>"><?= $make['make_name'] ?></option>
@@ -52,14 +52,14 @@
                     </br>
 
                     <label class="control-label">Aasta:</label>
-                    <select>
+                    <select name="year_id">
                         <option selected="selected" disabled="disabled">-kõik-</option>
                         <? foreach ($year_index as $year_type): ?>
                             <option value="<?= $year_type['year_id'] ?>"><?= $year_type['year_name'] ?></option>
                         <? endforeach ?>
                     </select>
                     <label class="control-label">kuni</label>
-                    <select>
+                    <select name="year_id">
                         <option selected="selected" disabled="disabled">-kõik-</option>
                         <? foreach ($year_index as $year_type): ?>
                             <option value="<?= $year_type['year_id'] ?>"><?= $year_type['year_name'] ?></option>
@@ -68,7 +68,7 @@
                     </br>
 
                     <label class="control-label">Kütus:</label>
-                    <select>
+                    <select name="fuel_type_id">
                         <option selected="selected" disabled="disabled">-kõik-</option>
                         <? foreach ($fuel_type_index as $fuel_type): ?>
                             <option value="<?= $fuel_type['fuel_type_id'] ?>"><?= $fuel_type['fuel_type_name'] ?></option>
@@ -77,7 +77,7 @@
                     </br>
 
                     <label class="control-label">Uste arv:</label>
-                    <select>
+                    <select name="door_id">
                         <option selected="selected" disabled="disabled">-kõik-</option>
                         <? foreach ($door_index as $door_type): ?>
                             <option value="<?= $door_type['door_id'] ?>"><?= $door_type['door_name'] ?></option>
@@ -87,12 +87,12 @@
 
                     <label class="control-label">Hind (€):</label>
                     <br>
-                    <input placeholder="näide: 300">
+                    <input name ="price_id" placeholder="näide: 300">
                     <? foreach ($price_index as $price_type): ?>
                         <input value="<?= $price_type['price_id'] ?>"><?= $price_type['price_name'] ?>
                     <? endforeach ?>
                     <label class="control-label">kuni</label>
-                    <input placeholder="näide: 49000">
+                    <input name = "price_id" placeholder="näide: 49000">
                     <? foreach ($price_index as $price_type): ?>
                         <input value="<?= $price_type['price_id'] ?>"><?= $price_type['price_name'] ?>
                     <? endforeach ?>
@@ -100,14 +100,14 @@
 
                     <label class="control-label">Sõiduki läbisõit (km):</label>
                     <br>
-                    <input placeholder="näide: 85000">
+                    <input name = "km_id" placeholder="näide: 85000">
                     <? foreach ($km_index as $km_type): ?>
                         <div class="input-group">
                             <input value="<?= $km_type['km_id'] ?>"><?= $km_type['km_name'] ?>
                         </div>
                     <? endforeach ?>
                     <label class="control-label">kuni</label>
-                    <input placeholder="näide: 380000">
+                    <input name = "km_id" placeholder="näide: 380000">
                     <? foreach ($km_index as $km_type): ?>
                         <div class="input-group">
                             <input value="<?= $km_type['km_id'] ?>"><?= $km_type['km_name'] ?>
@@ -117,14 +117,14 @@
 
                     <label class="control-label">Mootori võimsus (kW):</label>
                     <br>
-                    <input placeholder="näide: 92">
+                    <input name = "kw_id" placeholder="näide: 92">
                     <? foreach ($kw_index as $kw_type): ?>
                         <div class="input-group">
                             <input value="<?= $kw_type['kw_id'] ?>"><?= $kw_type['kw_name'] ?>
                         </div>
                     <? endforeach ?>
                     <label class="control-label">kuni</label>
-                    <input placeholder="näide:132">
+                    <input name ="kw_id" placeholder="näide:132">
                     <? foreach ($kw_index as $kw_type): ?>
                         <div class="input-group">
                             <input value="<?= $kw_type['kw_id'] ?>"><?= $kw_type['kw_name'] ?>
@@ -133,7 +133,7 @@
                     </br>
 
                     <label class="control-label">Käigukast:</label>
-                    <select>
+                    <select name="gearbox_id">
                         <option selected="selected" disabled="disabled">-kõik-</option>
                         <? foreach ($gearbox_index as $gearbox_type): ?>
                             <option value="<?= $gearbox_type['gearbox_id'] ?>"><?= $gearbox_type['gearbox_name'] ?></option>
@@ -142,7 +142,7 @@
                     </br>
 
                     <label class="control-label">Vedav sild:</label>
-                    <select>
+                    <select name="motion_id">
                         <option selected="selected" disabled="disabled">-kõik-</option>
                         <? foreach ($motion_index as $motion_type): ?>
                             <option value="<?= $motion_type['motion_id'] ?>"><?= $motion_type['motion_name'] ?></option>
@@ -150,14 +150,14 @@
                     </select>
                     </br>
                     <label class="control-label">Reg.number:</label>
-                    <input placeholder="näide:123ABC">
+                    <input name = "vin_id" placeholder="näide:123ABC">
                     <? foreach ($reg_index as $reg_type): ?>
                         <input value="<?= $vin_type['vin_id'] ?>"><?= $vin_type['vin_name'] ?>
                     <? endforeach ?>
                     </br>
 
                     <label class="control-label">VIN-kood:</label>
-                    <input>
+                    <input name="vin_id">
                     <? foreach ($vin_index as $vin_type): ?>
                         <div class="input-group">
                             <input value="<?= $vin_type['vin_id'] ?>"><?= $vin_type['vin_name'] ?>
@@ -166,7 +166,7 @@
                     </br>
 
                     <label class="control-label">Toodud riigist:</label>
-                    <select>
+                    <select name="country_id">
                         <option selected="selected" disabled="disabled">-kõik-</option>
                         <? foreach ($country_index as $country_type): ?>
                             <option value="<?= $country_type['country_id'] ?>"><?= $country_type['country_name'] ?></option>
@@ -175,7 +175,7 @@
                     </br>
 
                     <label class="control-label">Sõiduki asukoht:</label>
-                    <select>
+                    <select name="city_id">
                         <option selected="selected" disabled="disabled">-kõik-</option>
                         <? foreach ($city_index as $city_type): ?>
                             <option value="<?= $city_type['city_id'] ?>"><?= $city_type['city_name'] ?></option>
@@ -183,7 +183,7 @@
                     </select>
         </br>
         </br>
-        <a href="<?= BASE_URL ?>" button type="submit" class="btn btn-large btn-primary">Otsi</button></a>
+        <input type="submit" class="btn btn-large btn-primary" value="otsi">
         </form>
     </div>
 </div>
