910ae662d988199eb0de894b8c0a76b508efebbf
tanel.kolga <tanel.kolga@khk.ee>
2014-03-13 15:36:39 +0200

    Add "muuda kuulutus" and "kustuta kuulutus"

--- views/minukuulutused/minukuulutused_index.php
+++ views/minukuulutused/minukuulutused_index.php
@@ -1,8 +1,74 @@
 <form id="form">
-<div class="container">
-<div class="modal-header">
-	<a id="close" href="<?= BASE_URL ?>" class="close" data-dismiss="modal" aria-hidden="true">×</button></a>
+    <div class="container">
+        <div class="modal-header">
+            <a id="close" href="<?= BASE_URL ?>" class="close" data-dismiss="modal" aria-hidden="true">×</button></a>
 
-	<h3>Minu kuulutused</h3>
-</div>
-    <p>Sul puuduvad kuulutused</p>
+            <h3>Minu kuulutused</h3>
+        </div>
+
+        <div class="row">
+
+            <div class="span12">
+
+                <h4>
+                    <a href="#">Audi A4 2.0</a>
+                </h4>
+
+                <div class="clearfix">
+                    <img src="http://www.upload.ee/image/3940426/Audi_A4_B8_Avant_rear_20100715.jpg"
+                         alt="ALT NAME" class="pull-left clearfix">
+
+                </div>
+
+                <b>Keretüüp:</b>
+                <small>Universaal</small>
+                <br>
+                <b>Mark:</b> Audi</small>
+                <br>
+                <b>Mudel:</b> A4</small>
+                <br>
+                <b>Mootor</b> 2.0</small>
+                <br>
+                <b>VIN-kood: </b>WV2ZZZ70ZWH133657</small>
+                <br>
+                <b>Aasta:</b> 2003</small>
+                <br>
+                <b>Värv:</b> Must</small>
+                <br>
+                <b>Sõiduki läbisõit(km): </b>223 489</small>
+                <br>
+                <b>Kütus:</b> Diisel</small>
+                <br>
+                <b>Uste arv:</b> 5</small>
+                <br>
+                <b>Hind:</b> 4100</small>
+                <br>
+                <b>Käigukast: </b>Manuaal</small>
+                <br>
+                <b>Vedav sild: </b> Esivedu</small>
+                <br>
+                <b>Mootori võimsus(kw): </b>88</small>
+                <br>
+                <b>Toodud riigist: </b>Saksamaa</small>
+                <br>
+                <b>Sõiduki asukoht: </b>Tartu</small>
+                <br>
+                <b>Muu: </b> Sõiduk on väga heas korras, tulge ostke ja sõitke ära!</small>
+                <br>
+                <br>
+
+
+            </div>
+            <div class="caption" class="pull-left">
+
+                <a href="welcome" class="btn btn-success" pull-right">Muuda kuulutust</a>
+
+                <a href="welcome" class="btn btn-success" pull-right">Kustuta kuulutus</a>
+
+            </div>
+
+            </ul>
+        </div>
+    </div>
+    </div>
+</form>
